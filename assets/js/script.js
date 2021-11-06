44bfc157901267f80729eec222a530ab1a02400a
Brandon Hoskins <branjames117@gmail.com>
2021-11-05 22:17:30 -0500

    edit button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,12 @@ $(function () {
   // let currentHour = moment().format('H');
   let currentHour = 12;
 
-  // default array of time rows
+  // default array of time rows with no user input
   let defaultHoursArr = [
     { label: '9AM', mil: 9, data: '' },
     { label: '10AM', mil: 10, data: '' },
     { label: '11AM', mil: 11, data: '' },
-    { label: '12PM', mil: 12, data: 'Call the boss.' },
+    { label: '12PM', mil: 12, data: '' },
     { label: '1PM', mil: 13, data: '' },
     { label: '2PM', mil: 14, data: '' },
     { label: '3PM', mil: 15, data: '' },
@@ -26,75 +26,76 @@ $(function () {
     ? (hours = JSON.parse(localStorage.hours))
     : localStorage.setItem('hours', JSON.stringify(defaultHoursArr));
 
-  // for each hour in the standard work day, create a row
-  function renderHours() {
-    for (let i = 0; i < hours.length; i++) {
-      // create the entire row first
-      const hourRowEl = $('<div>').addClass('row justify-content-center');
+  // for each hour in the standard work day, create a row, using data from the hours array pulled from localstorage
+  for (let i = 0; i < hours.length; i++) {
+    // create the entire row first
+    const hourRowEl = $('<div>').addClass('row justify-content-center');
 
-      // create the left-column which displays the hour
-      const timeRowEl = $('<div>')
-        .addClass('col-2 col-md-1 p-3 border-top')
-        .text(hours[i].label);
+    // create the left-column which displays the hour
+    const timeRowEl = $('<div>')
+      .addClass('col-2 col-md-1 p-3 border-top')
+      .text(hours[i].label);
 
-      // create the middle-column which displays the user input
-      const entryRowEl = $('<div>')
-        .addClass('col-7 col-md-8 p-3 border hour-row')
-        // did the hour pass, if so, give bg-secondary
-        // is the hour current, if so, give bg-danger
-        // is the hour yet to pass, if so, give bg-success
-        .addClass(
-          hours[i].mil == currentHour
-            ? 'bg-success'
-            : hours[i].mil < currentHour
-            ? 'bg-secondary'
-            : 'bg-danger'
-        )
-        .attr('id', 'hour' + hours[i].mil);
-      const entryRowPEl = $('<p>').text(hours[i].data);
-      entryRowEl.append(entryRowPEl);
+    // create the middle-column which displays the user input
+    const entryRowEl = $('<div>')
+      .addClass('col-7 col-md-8 p-3 border hour-row')
+      // did the hour pass, if so, give bg-secondary
+      // is the hour current, if so, give bg-danger
+      // is the hour yet to pass, if so, give bg-success
+      .addClass(
+        hours[i].mil == currentHour
+          ? 'bg-success'
+          : hours[i].mil < currentHour
+          ? 'bg-secondary'
+          : 'bg-danger'
+      )
+      .attr('id', 'hour' + hours[i].mil);
+    const entryRowPEl = $('<p>').text(hours[i].data);
+    entryRowEl.append(entryRowPEl);
 
-      // create the right-column which displays the edit btn
-      const editRowEl = $('<div>').addClass(
-        'col-1 p-3 border rounded-right bg-info d-flex align-items-center justify-content-center'
-      );
-      const editButtonEl = $('<i>').addClass('fas fa-edit');
-      editRowEl.append(editButtonEl);
-      editRowEl.on('click', editButtonHandler);
+    // create the right-column which displays the edit btn
+    const editRowEl = $('<div>')
+      .addClass(
+        'col-1 p-3 border rounded-right bg-info d-flex align-items-center justify-content-center hour-edit'
+      )
+      .attr('id', 'edit' + hours[i].mil);
+    const editButtonEl = $('<i>').addClass('fas fa-edit');
+    editRowEl.append(editButtonEl);
 
-      // add it all together
-      hourRowEl.append(timeRowEl, entryRowEl, editRowEl);
+    // add it all together
+    hourRowEl.append(timeRowEl, entryRowEl, editRowEl);
 
-      // throw the finished row onto the container
-      $('.container').append(hourRowEl);
-    }
+    // throw the finished row onto the container
+    $('.container').append(hourRowEl);
   }
 
-  function editButtonHandler(e) {
-    $('#hour12').replaceWith('<input>').addClass('bg-green');
-  }
-
+  // update local storage any time the user enters new data
   function saveHours() {
     localStorage.setItem('hours', JSON.stringify(hours));
   }
 
-  renderHours();
-
+  // click listener to the text block in each row to enable user editing
   $('.hour-row').on('click', 'p', function () {
-    console.log('clicked');
     let currData = $(this).text().trim();
     let dataInput = $('<textarea>').val(currData);
     $(this).replaceWith(dataInput);
     dataInput.trigger('focus');
   });
 
+  // blur listener, to save user inputted data to local storage
   $('.hour-row').on('blur', 'textarea', function () {
-    console.log('blurred');
     let newData = $(this).val().trim();
     let dataIndex = $(this).parent().attr('id').replace('hour', '') - 9;
     hours[dataIndex].data = newData;
     saveHours();
     let newEntryRowPEl = $('<p>').text(newData);
     $(this).replaceWith(newEntryRowPEl);
+  });
+
+  // click listener for edit buttons
+  $('.hour-edit').on('click', function () {
+    // get ID to figure out which text field to edit
+    let hourToEdit = $(this).attr('id').replace('edit', '');
+    $('#hour' + hourToEdit + ' p').click();
   });
 });
